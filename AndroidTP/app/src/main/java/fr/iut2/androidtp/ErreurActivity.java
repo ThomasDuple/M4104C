f17b70d0e70a7d41b68f88e0e6b291652f82c2b0
ThomasDuple <thomasduple@gmail.com>
2021-03-03 13:08:29 +0100

    tp2

---
+++ AndroidTP/app/src/main/java/fr/iut2/androidtp/ErreurActivity.java
@@ -0,0 +1,14 @@
+package fr.iut2.androidtp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ErreurActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erreur);
+    }
+}
---
+++ AndroidTP/app/src/main/java/fr/iut2/androidtp/HelloActivity.java
@@ -0,0 +1,4 @@
+package fr.iut2.androidtp;
+
+public class HelloActivity {
+}
